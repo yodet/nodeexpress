49733b4c2bd2c82272673228325728fb93915cf7
yodet <82468898+yodet@users.noreply.github.com>
2021-08-18 20:26:05 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-var http = require("http");
-http.createServer(function(req,res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Aloha world');
-}).listen(process.env.PORT || 3000);
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
